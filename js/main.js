70f54d7766878e884785417a6902d6ac782e2c7c
Hannah Helms <helmshannahpatrice@gmail.com>
2017-09-23 10:36:11 -0400

    Step 5 for JS homework
    Added console.logs for other variables to prevent errors in developer
    tools.

---
+++ js/main.js
@@ -0,0 +1,23 @@
+var president1 = {firstName: 'Donald', lastName: 'Trump', termLength: '8 months', party: 'Republican', yearsOfPresidency: 0};
+
+var president2 = {firstName: 'Barrack', lastName: 'Obama', termLength: '8 Years', party: 'Democratic', yearsOfPresidency: 8};
+
+var president3 = {firstName: 'George W.', lastName: 'Bush', termLength: '8 Years', party: 'Republican', yearsOfPresidency: 8};
+
+var president4 = {firstName: 'William', lastName: 'Clinton', termLength: '8 Years', party: 'Democratic', yearsOfPresidency: 8};
+
+var president5 = {firstName: 'George H. W.', lastName: 'Bush', termLength: '4 Years', party: 'Republican', yearsOfPresidency: 4};
+
+var presidents = ['president1', 'president2', 'president3', 'president4', 'president5'];
+
+console.log(president3.firstName + ' ' + president3.lastName);
+
+console.log(president1.firstName + ' ' + president1.lastName);
+
+console.log(president2.firstName + ' ' + president2.lastName);
+
+console.log(president4.firstName + ' ' + president4.lastName);
+
+console.log(president5.firstName + ' ' + president5.lastName);
+
+console.log(presidents.length);
